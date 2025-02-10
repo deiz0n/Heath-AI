5ff79bf2eb7aaeb97b7d6a0b6707c2d4024ad6ff
deiz0n <eduardomacedosilva@outlook.com>
2025-02-10 15:53:44 -0300

    fix: correções nos models

--- web/models.py
+++ web/models.py
@@ -1,26 +1,39 @@
+from django.contrib import admin
 from django.db import models
 import uuid
 
 
-class ImagemRaioX(models.Model):
-    pd_direito_cima = models.ImageField(upload_to='imagens/raiox/')
-    pd_direito_lateral = models.ImageField(upload_to='imagens/raiox/')
-    pe_esquerdo_cima = models.ImageField(upload_to='imagens/raiox/')
-    pe_esquerdo_lateral = models.ImageField(upload_to='imagens/raiox/')
-    ambos_cima = models.ImageField(upload_to='imagens/raiox/')
-
-class ImagemRessonancia(models.Model):
-    pd_direito_cima = models.ImageField(upload_to='imagens/ressonancia/')
-    pd_direito_lateral = models.ImageField(upload_to='imagens/ressonancia/')
-    pe_esquerdo_cima = models.ImageField(upload_to='imagens/ressonancia/')
-    pe_esquerdo_lateral = models.ImageField(upload_to='imagens/ressonancia/')
-    ambos_cima = models.ImageField(upload_to='imagens/ressonancia/')
-
-class Documento(models.Model):
+class RaioX(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
-    raio_x = models.OneToOneField(ImagemRaioX, on_delete=models.CASCADE)
-    ressonancia = models.OneToOneField(ImagemRessonancia, on_delete=models.CASCADE)
+    img_pd_cima = models.ImageField(upload_to="imagens/raiox")
+    img_pd_lateral = models.ImageField(upload_to="imagens/raiox")
+    img_pe_cima = models.ImageField(upload_to="imagens/raiox")
+    img_pe_lateral = models.ImageField(upload_to="imagens/raiox")
+    img_ambos_cima = models.ImageField(upload_to="imagens/raiox")
+    
+    def __str__(self):
+        return str(self.id)
+
+class Ressonancia(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    img_pd_cima = models.ImageField(upload_to="imagens/ressonancia/")
+    img_pd_lateral = models.ImageField(upload_to="imagens/ressonancia/")
+    img_pe_cima = models.ImageField(upload_to="imagens/ressonancia/")
+    img_pe_lateral = models.ImageField(upload_to="imagens/ressonancia/")
+    img_ambos_cima = models.ImageField(upload_to="imagens/ressonancia/")
+    
+    def __str__(self):
+        return str(self.id)
+
+class MultiModal(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    raio_x = models.OneToOneField(RaioX, on_delete=models.CASCADE, related_name='multimodal')
+    ressonancia = models.OneToOneField(Ressonancia, on_delete=models.CASCADE, related_name='multimodal')
     data = models.DateTimeField(auto_now_add=True)
     
     def __str__(self):
-        return self.id
+        return str(self.id)
+
+admin.site.register(MultiModal)
+admin.site.register(RaioX)
+admin.site.register(Ressonancia)
