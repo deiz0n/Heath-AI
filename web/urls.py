e664832d092aedc329594eefdea59db316e1ce0b
deiz0n <eduardomacedosilva@outlook.com>
2025-02-12 09:53:42 -0300

    feat: criação da rota da página inicial

--- web/urls.py
+++ web/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 
-from .views import criar_multi_modal, buscar_multi_modais
+from .views import criar_multi_modal, buscar_multi_modais, pagina_inicial
 
 urlpatterns = [
     path('documentos/criar/', criar_multi_modal),
     path('documentos/', buscar_multi_modais),
+    path('', pagina_inicial)
 ]
