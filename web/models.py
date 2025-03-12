01307d45d738490e3da4689119a9e7c9ffca5ad5
deiz0n <eduardomacedosilva@outlook.com>
2025-03-12 14:48:17 -0300

    feat: adição campo de prontuário aos modelos RaioX, Ressonancia e MultiModal

--- web/models.py
+++ web/models.py
@@ -5,11 +5,12 @@ import uuid
 
 class RaioX(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
-    img_pd_cima = models.ImageField(upload_to="imagens/raiox")
-    img_pd_lateral = models.ImageField(upload_to="imagens/raiox")
-    img_pe_cima = models.ImageField(upload_to="imagens/raiox")
-    img_pe_lateral = models.ImageField(upload_to="imagens/raiox")
-    img_ambos_cima = models.ImageField(upload_to="imagens/raiox")
+    img_pd_cima = models.ImageField(upload_to="imagens/raiox/")
+    img_pd_lateral = models.ImageField(upload_to="imagens/raiox/")
+    img_pe_cima = models.ImageField(upload_to="imagens/raiox/")
+    img_pe_lateral = models.ImageField(upload_to="imagens/raiox/")
+    img_ambos_cima = models.ImageField(upload_to="imagens/raiox/")
+    prontuario = models.FileField(upload_to="prontuario/raiox", default="Prontuário não informado", null=True)
     
     def __str__(self):
         return str(self.id)
@@ -21,6 +22,7 @@ class Ressonancia(models.Model):
     img_pe_cima = models.ImageField(upload_to="imagens/ressonancia/")
     img_pe_lateral = models.ImageField(upload_to="imagens/ressonancia/")
     img_ambos_cima = models.ImageField(upload_to="imagens/ressonancia/")
+    prontuario = models.FileField(upload_to="prontuario/ressonancia", default="Prontuário não informado", null=True)
     
     def __str__(self):
         return str(self.id)
@@ -30,6 +32,7 @@ class MultiModal(models.Model):
     raio_x = models.OneToOneField(RaioX, on_delete=models.CASCADE, related_name='multimodal')
     ressonancia = models.OneToOneField(Ressonancia, on_delete=models.CASCADE, related_name='multimodal')
     data = models.DateTimeField(auto_now_add=True)
+    prontuario = models.FileField(upload_to="prontuario/geral", default="Prontuário não informado", null=True)
     
     def __str__(self):
         return str(self.id)
