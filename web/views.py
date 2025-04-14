18bfa9903e2599a0abeab3d3c7fd6d9b525cb8ee
deiz0n <eduardomacedosilva@outlook.com>
2025-04-14 15:34:32 -0300

    feat: criação da view upload-multi-modal

--- web/views.py
+++ web/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, redirect
 
-from .models import RaioX, Ressonancia, ImagensRessonancia
+from .models import RaioX, Ressonancia, ImagensRessonancia, MultiModal
 
 def upload_raio_x(request):
     
@@ -51,7 +51,39 @@ def upload_ressonancia(request):
         print(f"Erro ao processar formulário: {str(e)}") 
         return render(request, 'web/pages/pagina-inicial.html')
 
-    
+
+def upload_multi_modal(request):
+    try:
+        files = request.FILES
+
+        if 'ambos_img_pd_cima' in files and 'ambos_ressonancia' in files:
+            novo_raiox = RaioX.objects.create(
+                img_pd_cima=files.get('ambos_img_pd_cima'),
+                img_pd_lateral=files.get('ambos_img_pd_lateral'),
+                img_pe_cima=files.get('ambos_img_pe_cima'),
+                img_pe_lateral=files.get('ambos_img_pe_lateral'),
+                img_ambos_cima=files.get('ambos_img_ambos_cima'),
+                prontuario=files.get('prontuario')
+            )
+
+            nova_ressonancia = Ressonancia.objects.create()
+            imagens = [
+                ImagensRessonancia(ressonancia=nova_ressonancia, imagem=imagem)
+                for imagem in files.getlist('ambos_ressonancia')
+            ]
+            ImagensRessonancia.objects.bulk_create(imagens)
+
+            novo_multimodal = MultiModal.objects.create(
+                raio_x=novo_raiox,
+                ressonancia=nova_ressonancia,
+                prontuario=files.get('prontuario')
+            )
+
+            return render(request, 'web/pages/pagina-inicial.html', {'success': True})
+
+    except Exception as e:
+        print(f"Erro ao salvar multimodal: {str(e)}")
+        return render(request, 'web/pages/pagina-inicial.html', {'error': str(e)})
 
 def pagina_inicial(request):
     return render(request, 'web/pages/pagina-inicial.html')
