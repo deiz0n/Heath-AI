5c9e2852bc846b9733978d2793c6e7bb2623641e
deiz0n <eduardomacedosilva@outlook.com>
2025-04-14 15:34:50 -0300

    feat: criação da url upload-multi-modal

--- web/urls.py
+++ web/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 
-from .views import pagina_inicial, upload_raio_x, upload_ressonancia
+from .views import pagina_inicial, upload_raio_x, upload_ressonancia, upload_multi_modal
 
 urlpatterns = [
     path('', pagina_inicial, name='home'),
     path('upload-raiox/', upload_raio_x, name='upload-raiox'),
-    path('upload-ressonancia/', upload_ressonancia, name='upload-ressonancia')
+    path('upload-ressonancia/', upload_ressonancia, name='upload-ressonancia'),
+    path('upload-multi-modal/', upload_multi_modal, name='upload-multi-modal')
 ]
