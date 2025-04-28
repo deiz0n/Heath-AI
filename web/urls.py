b5de8d2abd609fd7c5f3de46c95bc80fc7a5d48d
deiz0n <eduardomacedosilva@outlook.com>
2025-04-28 09:49:13 -0300

    feat: criação da url para acesso a página de cadastro de usuário

--- web/urls.py
+++ web/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
 
-from .views import pagina_inicial, upload_raio_x, upload_ressonancia, upload_multi_modal, login
+from .views import pagina_inicial, upload_raio_x, upload_ressonancia, upload_multi_modal, login, cadastro_usuario
 
 urlpatterns = [
     path('', login, name='login'),
     path('pagina-inicial/', pagina_inicial, name='home'),
+    path('cadastro-usuario/', cadastro_usuario,name='cadastro-usuario'),
     path('upload-raiox/', upload_raio_x, name='upload-raiox'),
     path('upload-ressonancia/', upload_ressonancia, name='upload-ressonancia'),
     path('upload-multi-modal/', upload_multi_modal, name='upload-multi-modal')
