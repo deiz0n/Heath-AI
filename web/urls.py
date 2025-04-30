af7409a740b1e430ffa5f27ccd452b31f3fa774a
deiz0n <eduardomacedosilva@outlook.com>
2025-04-30 08:13:28 -0300

    feat: implementação do registro de usuários e gerenciamento de sessões

--- web/urls.py
+++ web/urls.py
@@ -1,11 +1,13 @@
 from django.urls import path
 
-from .views import pagina_inicial, upload_raio_x, upload_ressonancia, upload_multi_modal, login, cadastro_usuario
+from .views import pagina_inicial, upload_raio_x, upload_ressonancia, upload_multi_modal, login_view, cadastro_usuario, criar_usuario, iniciar_sessao
 
 urlpatterns = [
-    path('', login, name='login'),
+    path('', login_view, name='login'),
     path('pagina-inicial/', pagina_inicial, name='home'),
+    path('criar-usuario/', criar_usuario, name='criar-usuario'),
     path('cadastro-usuario/', cadastro_usuario,name='cadastro-usuario'),
+    path('iniciar-sessao/', iniciar_sessao, name='iniciar-sessao'),
     path('upload-raiox/', upload_raio_x, name='upload-raiox'),
     path('upload-ressonancia/', upload_ressonancia, name='upload-ressonancia'),
     path('upload-multi-modal/', upload_multi_modal, name='upload-multi-modal')
--- web/views.py
+++ web/views.py
@@ -1,7 +1,62 @@
 from django.shortcuts import render, redirect
+from django.contrib.auth.hashers import make_password
+from django.contrib.auth.models import User
+from django.contrib.auth import authenticate, login
+from django.contrib.auth.decorators import login_required
 
-from .models import RaioX, Ressonancia, ImagensRessonancia, MultiModal
+from .models import RaioX, Ressonancia, ImagensRessonancia, MultiModal, Clinico
 
+def criar_usuario(request):
+    try:
+        if request.method == 'POST':    
+            clinico = Clinico.objects.create(
+                nome=request.POST.get('nome'),
+                sobrenome=request.POST.get('sobrenome'),
+                cpf=request.POST.get('cpf'),
+                crm=request.POST.get('crm'),
+                data_aniversario=request.POST.get('data_aniversario'),
+                email=request.POST.get('email'),
+                senha=make_password(request.POST.get('senha'))
+            )
+            
+            user = User.objects.create_user(
+                username=clinico.email,
+                password=request.POST.get('senha-cadastro'),
+                email=clinico.email
+            )
+                        
+            clinico.save()
+            return redirect('login')
+        else:
+            return redirect('login')    
+        
+    except Exception as e:
+        print(f"Erro ao processar formulário: {str(e)}")
+        return render(request, 'web/pages/login.html')
+    
+
+def iniciar_sessao(request):
+    try:
+        if request.method == "POST":
+            username = request.POST.get("email")
+            password = request.POST.get("senha")
+            user = authenticate(request, username=username, password=password)
+
+            if user is not None:
+                login(request, user)
+                return redirect('home')
+            else:
+                return redirect('login') 
+            
+        return redirect('login')
+    
+    except Exception as e:
+        print(f"Erro ao processar formulário: {str(e)}")
+        return render(request, 'web/pages/login.html')
+
+    
+        
+@login_required
 def upload_raio_x(request):
     
     try:
@@ -27,7 +82,7 @@ def upload_raio_x(request):
         print(f"Erro ao processar formulário: {str(e)}")
         return render(request, 'web/pages/pagina-inicial.html')
 
-
+@login_required
 def upload_ressonancia(request):
     try:
         files = request.FILES
@@ -51,10 +106,13 @@ def upload_ressonancia(request):
         print(f"Erro ao processar formulário: {str(e)}") 
         return render(request, 'web/pages/pagina-inicial.html')
 
-
+@login_required
 def upload_multi_modal(request):
     try:
         files = request.FILES
+
+        usuario = request.user
+        clinico = Clinico.objects.get(email=usuario.email)
 
         if 'ambos_img_pd_cima' in files and 'ambos_ressonancia' in files:
             novo_raiox = RaioX.objects.create(
@@ -75,7 +133,8 @@ def upload_multi_modal(request):
             novo_multimodal = MultiModal.objects.create(
                 raio_x=novo_raiox,
                 ressonancia=nova_ressonancia,
-                prontuario=files.get('prontuario')
+                prontuario=files.get('prontuario'),
+                clinico=clinico
             )
 
             return render(request, 'web/pages/pagina-inicial.html', {'success': True})
@@ -84,10 +143,11 @@ def upload_multi_modal(request):
         print(f"Erro ao salvar multimodal: {str(e)}")
         return render(request, 'web/pages/pagina-inicial.html', {'error': str(e)})
 
+@login_required
 def pagina_inicial(request):
     return render(request, 'web/pages/pagina-inicial.html')
 
-def login(request):
+def login_view(request):
     return render(request, 'web/pages/login.html')
 
 def cadastro_usuario(request):
