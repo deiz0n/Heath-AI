fbfbf4f057a4378fcf79a830b96f4e0aac4a7c7a
deiz0n <eduardomacedosilva@outlook.com>
2025-05-09 10:39:04 -0300

    fix: ajuste ao verificar se o input de senha é do tipo password

--- web/static/web/js/login.js
+++ web/static/web/js/login.js
@@ -3,6 +3,6 @@ const checkboxMostrarSenha = document.querySelector('#mostrar-senha');
 
 if (checkboxMostrarSenha)
     checkboxMostrarSenha.addEventListener('change', () => {
-        if (inputSenha.type == 'password') inputSenha.type = 'text';
+        if (inputSenha.type === 'password') inputSenha.type = 'text';
         else inputSenha.type = 'password';
     });
