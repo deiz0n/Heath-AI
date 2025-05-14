cbd959fe2dae6d35dde5d12268f18bb53988cfc8
deiz0n <eduardomacedosilva@outlook.com>
2025-05-14 18:12:07 -0300

    feat: Criação do ModelForm para todos os models

---
+++ web/forms.py
@@ -0,0 +1,29 @@
+from django.forms import ModelForm
+from .models import RaioX, ImagensRessonancia, Ressonancia, Clinico, MultiModal
+
+class RaioXForm(ModelForm):
+    class Meta:
+        model = RaioX
+        fields = ["img_pd_cima", "img_pd_lateral", "img_pe_cima", "img_pe_lateral", "img_ambos_cima", "prontuario"]
+
+class ImagensRessonanciaForm(ModelForm):
+    class Meta:
+        model = ImagensRessonancia
+        fields = ["imagem"]
+
+class RessonanciaForm(ModelForm):
+    class Meta:
+        model = Ressonancia
+        fields = ["imagens", "prontuario"]
+
+class ClinicoForm(ModelForm):
+    class Meta:
+        model = Clinico
+        fields = ["nome", "sobrenome", "cpf", "crm", "data_aniversario", "email", "senha"]
+
+class MultiModalForm(ModelForm):
+    class Meta:
+        model = MultiModal
+        fields = ["raio_x", "ressonancia", "data", "prontuario", "clinico"]
+
+
