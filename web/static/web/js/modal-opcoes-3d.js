de369301d8c6c33d06bb9aa97de8f201f9d34389
deiz0n <eduardomacedosilva@outlook.com>
2025-05-21 09:08:27 -0300

    feat: centralização do css e script do modal de opcões 3D

---
+++ web/static/web/js/modal-opcoes-3d.js
@@ -0,0 +1,19 @@
+import { backgroundModal, modalAnalysisType } from "./modal-tipo-analise.js";
+import { closeCurrentModal } from "./script.js";
+
+const btn3d = document.querySelector('#btn-3d');
+const btnClose = document.querySelector('#modal-opcoes-container i');
+const modalOptions3D = document.querySelector('#modal-opcoes-container');
+
+if (btn3d) btn3d.addEventListener('click', openModalOptions3D);
+if (btnClose) btnClose.addEventListener('click', closeModalOptions3D);
+
+function openModalOptions3D() {
+    closeCurrentModal(modalAnalysisType.id)
+    modalOptions3D.style.display = 'block';
+}
+
+function closeModalOptions3D() {
+    modalOptions3D.style.display = 'none';
+    backgroundModal.classList.remove('show');
+}
