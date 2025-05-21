96314ed1be93bdb7b9fbe8eaafc16dba11f69c02
deiz0n <eduardomacedosilva@outlook.com>
2025-05-21 09:09:54 -0300

    refactor: remoção de funções não usadas

--- web/static/web/js/script.js
+++ web/static/web/js/script.js
@@ -3,20 +3,13 @@ import { startModalRessonancia } from "./modal-ressonancia.js";
 import { startModalAmbos } from "./modal-ambos.js";
 
 const btn2D = document.querySelector('#btn-2d');
-const btn3D = document.querySelector('#btn-3d');
 const btnStartRaioX = document.querySelectorAll('.btn-modal-raiox');
 const btnStartRessonancia = document.querySelectorAll('.btn-modal-ressonancia');
 const btnStartAmbos = document.querySelector('#btn-modal-ambos');
-// const btnStartProntuario = document.querySelector('#next-step');
-// const formComponent = document.querySelector('.form');
 
-// const modalRaioX = document.querySelector('#modal-raiox-container');
-
-const initModalOpcoes3D = (typeModal) => {
-    const modal = document.querySelector('#modal-opcoes-container');
-    modal.style.display = 'block';
-    closeCurrentModal(typeModal);
-}
+if (btnStartRaioX) btnStartRaioX.forEach(btn => btn.addEventListener('click', startModalRaioX));
+if (btnStartRessonancia) btnStartRessonancia.forEach(btn => btn.addEventListener('click', startModalRessonancia));
+if (btnStartAmbos) btnStartAmbos.addEventListener('click', startModalAmbos);
 
 const initModalOpcoes2D = (typeModal) => {
     const modal = document.querySelector('#modal-opcoes-2d-container');
@@ -24,34 +17,16 @@ const initModalOpcoes2D = (typeModal) => {
     closeCurrentModal(typeModal);
 }
 
-const initModalRessonancia = (typeModal) => {
-    const modal = document.querySelector('#modal-ressonancia-container');
-    modal.style.display = 'block';
-    closeCurrentModal(typeModal);
-}
-
-// const initModalAmbos = () => {
-//     const typeModal = 'ambos'
-//     startModal(typeModal);
-// }
+btn2D.addEventListener('click', () => initModalOpcoes2D('tipo-imagem'));
 
 export const initModalProntuario = (typeModal) => {
     const modalProntuario = document.querySelector('#modal-prontuario-container');
+
     modalProntuario.style.display = 'block';
     closeCurrentModal(typeModal)
 }
 
-btn3D.addEventListener('click', () => initModalOpcoes3D('tipo-imagem'));
-
-btn2D.addEventListener('click', () => initModalOpcoes2D('tipo-imagem'));
-
-btnStartRaioX.forEach(btn => btn.addEventListener('click', startModalRaioX));
-
-btnStartRessonancia.forEach(btn => btn.addEventListener('click', startModalRessonancia));
-
-btnStartAmbos.addEventListener('click', startModalAmbos);
-
-function closeCurrentModal(typeModal) {
-    const modal = document.querySelector(`#modal-${typeModal}-container`);
+export function closeCurrentModal(modalId) {
+    const modal = document.querySelector(`#${modalId}`);
     modal.style.display = 'none';
 }
