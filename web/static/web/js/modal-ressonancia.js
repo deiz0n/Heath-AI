ec28d8140af570c1f16fa6ca9e440e7a6a5e13c2
deiz0n <eduardomacedosilva@outlook.com>
2025-05-23 00:39:56 -0300

    refactor: reorganização e melhorias na lógica de abertura e fechamento do modal de ressonância

--- web/static/web/js/modal-ressonancia.js
+++ web/static/web/js/modal-ressonancia.js
@@ -1,30 +1,49 @@
-export function startModalRessonancia() {
-    const btnNextStep = document.querySelector('#btn-next-step-ressonancia');
+import { closeCurrentModal } from "./script.js";
+import { modalOptions2D } from "./modal-opcoes-2d.js";
+import { modalOptions3D } from "./modal-opcoes-3d.js";
+import { backgroundModal } from "./modal-tipo-analise.js";
 
-    const modalRessonancia = document.querySelector('#modal-ressonancia-container');
-    const initModalProntuarioRessonancia = document.querySelector('#modal-prontuario-container-ressonancia');
+const btnNextStep = document.querySelector('#btn-next-step-ressonancia');
+const btnOpenModalResonance = document.querySelectorAll('.btn-modal-ressonancia');
+const btnCloseModalResonance = document.querySelector('#modal-ressonancia-container i');
 
-    const input = document.querySelector('#input-ressonancia');
+const modalResonance = document.querySelector('#modal-ressonancia-container');
+const initModalProntuarioRessonancia = document.querySelector('#modal-prontuario-container-ressonancia');
+const input = document.querySelector('#input-ressonancia');
 
+
+if (input)
     input.addEventListener('change', (e) => {
         updateQuantityFiles(e.target.files.length);
     });
 
-    btnNextStep.addEventListener('click', initModalProntuario);
+if (btnOpenModalResonance)
+    btnOpenModalResonance
+        .forEach(btn => btn.addEventListener('click', openModalResonance))
 
-    initModal();
+if (btnCloseModalResonance) btnCloseModalResonance.addEventListener('click', closeModalResonance);
 
-    function initModal() {
-        modalRessonancia.style.display = 'block';
-    };
 
-    function updateQuantityFiles(quantity) {
-        const quantityFiles = document.querySelector('#valor-input-ressonancia span');
-        quantityFiles.innerText = `${quantity}`;
-    };
+if (btnNextStep) btnNextStep.addEventListener('click', initModalProntuario);
 
-    function initModalProntuario() {
-        initModalProntuarioRessonancia.style.display = 'block';
-        modalRessonancia.style.display = 'none';
-    }
-};
+function openModalResonance() {
+    if (modalOptions2D.style.display === 'block') closeCurrentModal(modalOptions2D.id);
+    if (modalOptions3D.style.display === 'block') closeCurrentModal(modalOptions3D.id);
+
+    modalResonance.style.display = 'block';
+}
+
+function closeModalResonance() {
+    modalResonance.style.display = 'none';
+    backgroundModal.classList.remove('show');
+}
+
+function updateQuantityFiles(quantity) {
+    const quantityFiles = document.querySelector('#valor-input-ressonancia span');
+    quantityFiles.innerText = `${quantity}`;
+}
+
+function initModalProntuario() {
+    initModalProntuarioRessonancia.style.display = 'block';
+    modalResonance.style.display = 'none';
+}
