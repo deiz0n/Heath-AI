d9f60c234e5299779a57f4700528519ea01d5020
deiz0n <eduardomacedosilva@outlook.com>
2025-05-23 00:40:11 -0300

    refactor: remoção de funções e eventos não utilizados

--- web/static/web/js/script.js
+++ web/static/web/js/script.js
@@ -1,27 +1,12 @@
 import { startModalRaioX } from "./modal-raiox.js";
-import { startModalRessonancia } from "./modal-ressonancia.js";
+
 import { startModalAmbos } from "./modal-ambos.js";
 
 const btnStartRaioX = document.querySelectorAll('.btn-modal-raiox');
-const btnStartRessonancia = document.querySelectorAll('.btn-modal-ressonancia');
 const btnStartAmbos = document.querySelector('#btn-modal-ambos');
 
 if (btnStartRaioX) btnStartRaioX.forEach(btn => btn.addEventListener('click', startModalRaioX));
-if (btnStartRessonancia) btnStartRessonancia.forEach(btn => btn.addEventListener('click', startModalRessonancia));
 if (btnStartAmbos) btnStartAmbos.addEventListener('click', startModalAmbos);
-
-const initModalOpcoes2D = (typeModal) => {
-    const modal = document.querySelector('#modal-opcoes-2d-container');
-    modal.style.display = 'block';
-    closeCurrentModal(typeModal);
-}
-
-export const initModalProntuario = (typeModal) => {
-    const modalProntuario = document.querySelector('#modal-prontuario-container');
-
-    modalProntuario.style.display = 'block';
-    closeCurrentModal(typeModal)
-}
 
 export function closeCurrentModal(modalId) {
     const modal = document.querySelector(`#${modalId}`);
