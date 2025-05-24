e5f859396a6e38de2a048c9413ae5e5b5645e7ce
deiz0n <eduardomacedosilva@outlook.com>
2025-05-24 19:27:04 -0300

    refactor: reorganização e melhorias na lógica de eventos do modal de informações de exame

--- web/static/web/js/modal-informacoes-exame.js
+++ web/static/web/js/modal-informacoes-exame.js
@@ -1,18 +1,28 @@
-const patientDataContainer = document.querySelectorAll('.patient-data');
-const modalInfoExam = document.querySelector('#modal-info-exam-container');
-const btnClose = document.querySelector('#modal-info-exam-container i');
+function initializeEventListeners() {
+    const patientDataContainer = document.querySelectorAll('.patient-data');
+    const modalInfoExam = document.querySelector('#modal-info-exam-container');
+    const btnClose = document.querySelector('#modal-info-exam-container i');
 
-if (patientDataContainer)
-    patientDataContainer.forEach(patientData => {
-        patientData.addEventListener('click', openModalInfoExam);
-    })
+    if (patientDataContainer) {
+        patientDataContainer.forEach(patientData => {
+            patientData.removeEventListener('click', openModalInfoExam);
+            patientData.addEventListener('click', openModalInfoExam);
+        });
+    }
 
-if (btnClose) btnClose.addEventListener('click', closeModalInfoExam);
+    if (btnClose) {
+        btnClose.removeEventListener('click', closeModalInfoExam);
+        btnClose.addEventListener('click', closeModalInfoExam);
+    }
 
-function openModalInfoExam() {
-    modalInfoExam.style.display = 'block'
+    function openModalInfoExam() {
+        modalInfoExam.style.display = 'block';
+    }
+
+    function closeModalInfoExam() {
+        modalInfoExam.style.display = 'none';
+    }
 }
 
-function closeModalInfoExam() {
-    modalInfoExam.style.display = 'none'
-}
+document.addEventListener('DOMContentLoaded', initializeEventListeners);
+document.body.addEventListener('htmx:afterSwap', initializeEventListeners);
