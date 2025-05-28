e87c0f63ba161da6f39e5dff4de0bc046478b76e
deiz0n <eduardomacedosilva@outlook.com>
2025-05-28 14:02:13 -0300

    feat: atualização da exibição de informações do paciente no modal

--- web/static/web/js/modal-informacoes-exame.js
+++ web/static/web/js/modal-informacoes-exame.js
@@ -4,6 +4,21 @@ function initializeEventListeners() {
     const btnClose = document.querySelector('#modal-info-exam-container i');
     const backgroundModal = document.querySelector('#modal-backdrop');
 
+    if (patientDataContainer.length) {
+        patientDataContainer.forEach(patientData => {
+            patientData.removeEventListener('click', openModalInfoExam);
+            patientData.addEventListener('click', function () {
+                setDataModalInfoExam(this);
+                openModalInfoExam();
+            });
+        });
+    }
+
+    if (btnClose) {
+        btnClose.removeEventListener('click', closeModalInfoExam);
+        btnClose.addEventListener('click', closeModalInfoExam);
+    }
+
     function openModalInfoExam() {
         modalInfoExam.style.display = 'block';
         backgroundModal.classList.add('show');
@@ -14,17 +29,28 @@ function initializeEventListeners() {
         backgroundModal.classList.remove('show');
     }
 
-    if (patientDataContainer) {
-        patientDataContainer.forEach(patientData => {
-            patientData.removeEventListener('click', openModalInfoExam);
-            patientData.addEventListener('click', openModalInfoExam);
-        });
+    function setDataModalInfoExam(patientData) {
+        const datasets = {
+            patientName: patientData.dataset.patientName,
+            patientCpf: patientData.dataset.patientCpf,
+            patientAge: patientData.dataset.patientAge,
+            patientAddress: patientData.dataset.patientAddress,
+            clinicianName: patientData.dataset.clinicianName,
+            clinicianCrm: patientData.dataset.clinicianCrm,
+            examType: patientData.dataset.examType,
+            examData: patientData.dataset.exam
+        };
+
+        document.querySelector(('#patient-name')).textContent = datasets.patientName;
+        document.querySelector(('#patient-cpf')).textContent = datasets.patientCpf;
+        document.querySelector(('#patient-age')).textContent = datasets.patientAge;
+        document.querySelector(('#patient-address')).textContent = datasets.patientAddress;
+        document.querySelector(('#clinician-name')).textContent = datasets.clinicianName;
+        document.querySelector(('#clinician-crm')).textContent = datasets.clinicianCrm;
+        document.querySelector(('#exam-type')).textContent = datasets.examType;
+        document.querySelector(('#exam-data')).textContent = datasets.examData;
     }
 
-    if (btnClose) {
-        btnClose.removeEventListener('click', closeModalInfoExam);
-        btnClose.addEventListener('click', closeModalInfoExam);
-    }
 }
 
 document.addEventListener('DOMContentLoaded', initializeEventListeners);
