c9c5e140530c00e21056310a2b5e5b094b88a736
deiz0n <eduardomacedosilva@outlook.com>
2025-06-17 09:42:10 -0300

    refactor: criação do modal para definir o tipo de visualização das imagens

--- web/static/web/js/modal-tipo-analise.js
+++ web/static/web/js/modal-tipo-analise.js
@@ -1,16 +1,10 @@
-import {closeCurrentModal} from "./script.js";
-
 function openModalAnalysisType() {
     const modalAnalysisType = document.querySelector('#modal-tipo-imagem-container');
     const backgroundModal = document.querySelector('#modal-backdrop');
-    const modalInfoExam = document.querySelector('#modal-info-exam-container');
-
     if (modalAnalysisType && backgroundModal) {
         modalAnalysisType.style.display = 'block';
         backgroundModal.classList.add('show');
     }
-
-    if (modalInfoExam) closeCurrentModal(modalInfoExam.id)
 }
 
 function closeModalAnalysisType() {
@@ -22,15 +16,9 @@ function closeModalAnalysisType() {
     }
 }
 
-function updateTitleModalAnalysisType() {
-    const titleModalAnalysisType = document.querySelector('#modal-tipo-imagem-container .modal-titulo');
-    titleModalAnalysisType.textContent = 'Selecione uma das opções para visualização:';
-}
-
 export function assignListenersModal() {
     const btnClose = document.querySelector('#modal-tipo-imagem-container i');
     const btnOpenModalAnalysisType = document.querySelector('#main-btn');
-    const btnOpenModalTypeShowView = document.querySelector('#modal-info-btn-show-images');
 
     if (btnClose) {
         btnClose.removeEventListener('click', closeModalAnalysisType);
@@ -39,11 +27,6 @@ export function assignListenersModal() {
     if (btnOpenModalAnalysisType) {
         btnOpenModalAnalysisType.removeEventListener('click', openModalAnalysisType);
         btnOpenModalAnalysisType.addEventListener('click', openModalAnalysisType);
-    }
-    if (btnOpenModalTypeShowView) {
-        updateTitleModalAnalysisType();
-        btnOpenModalTypeShowView.removeEventListener('click', openModalAnalysisType);
-        btnOpenModalTypeShowView.addEventListener('click', openModalAnalysisType);
     }
 }
 
---
+++ web/static/web/js/modal-tipo-visualizacao.js
@@ -0,0 +1,35 @@
+import {closeCurrentModal} from "./script.js";
+
+function initializeModalTypeView() {
+   const btnOpenModalTypeView = document.querySelector('#modal-info-btn-show-images');
+   const btnCloseModalTypeView = document.querySelector('#modal-tipo-visualizacao-container i');
+
+   const modalTypeView = document.querySelector('#modal-tipo-visualizacao-container');
+   const background = document.querySelector('#modal-backdrop');
+   const modalInfoExam = document.querySelector('#modal-info-exam-container');
+
+   console.log(modalTypeView)
+
+   if (btnOpenModalTypeView) {
+       btnOpenModalTypeView.removeEventListener('click', openModalTypeView)
+       btnOpenModalTypeView.addEventListener('click', openModalTypeView)
+   }
+
+   if (btnCloseModalTypeView) {
+       btnCloseModalTypeView.removeEventListener('click', closeModalTypeView)
+       btnCloseModalTypeView.addEventListener('click', closeModalTypeView)
+   }
+
+   function openModalTypeView() {
+       closeCurrentModal(modalInfoExam.id);
+       modalTypeView.style.display = 'block';
+   }
+
+   function closeModalTypeView() {
+       modalTypeView.style.display = 'none';
+       background.classList.remove('show');
+   }
+}
+
+document.addEventListener('DOMContentLoaded', initializeModalTypeView);
+document.body.addEventListener('htmx:afterSwap', initializeModalTypeView);
