e517388d33981898204a0e03fc2d7c68fea61d23
deiz0n <eduardomacedosilva@outlook.com>
2025-08-28 12:38:40 -0300

    refactor: streamline modal functionality and improve file upload handling for X-Ray and Resonance exams

--- web/static/web/js/modal-ambos.js
+++ web/static/web/js/modal-ambos.js
@@ -1,282 +1,186 @@
 import { closeCurrentModal } from "./script.js";
-import {
-    colorBlack,
-    colorBlue,
-    colorGreen,
-    iconPending,
-    iconCheck
-} from "./styles.js";
 
-export function startModalAmbos() {
-    const backgroundModal = document.querySelector('#modal-backdrop');
-
-    const modalAmbos = document.querySelector('#modal-ambos-container');
-    const modalProntuarioAmbos = document.querySelector('#modal-prontuario-container-ambos');
-    const modalRegisterPatient = document.querySelector('#modal-register-patient-container');
-
-    const labels = document.querySelectorAll('#modal-ambos-container [id^="label"]');
-    const inputs = document.querySelectorAll('#modal-ambos-container input[type=file]');
-    const fileNames = document.querySelectorAll('.input-value-ambos');
-
-    const btnOpenModalBoth = document.querySelector('#btn-submit-modal-register-patient');
-    const btnCloseModalBoth = document.querySelector('#btn-close-ambos');
-    const btnNextStep = document.querySelector('#btn-next-step-ambos');
-    const btnPrevStep = document.querySelector('#btn-prev-step-ambos');
-
-    const progressBar = document.querySelector('#modal-ambos-container #progress-bar');
-    const valueProgress = document.querySelector('#modal-ambos-container #progress-title span');
-    let stepProgressValue = parseInt(valueProgress.innerHTML) || 0;
-
-    const steps = document.querySelectorAll('#modal-ambos-container .form-step');
-    const totalSteps = steps.length - 1;
-    let currentStep = 0;
-
-    btnOpenModalBoth?.addEventListener('click', () => {
-        const modalTarget = btnOpenModalBoth.getAttribute('data-target-modal');
-        if (modalTarget === 'modal-both')
-            openModalBoth();
-    });
-    btnCloseModalBoth?.addEventListener('click', closeModaBoth);
-    btnNextStep?.addEventListener('click', nextElement);
-    btnPrevStep?.addEventListener('click', prevElement);
-
-    inputs?.forEach(input => {
-        input.addEventListener('change', () => {
-            updateFileName(currentStep);
-            console.log(input.id);
-        });
-    });
-
-    function openModalBoth() {
-        setInputPatientModalBoth();
-        resetComponents();
-        closeCurrentModal(modalRegisterPatient.id)
-        modalAmbos.style.display = 'block';
-        updateEventBtnPrev();
-        inputs[0].style.visibility = 'visible';
-    }
-
-    function closeModaBoth() {
-        modalAmbos.style.display = 'none';
-        backgroundModal.classList.remove('show');
-        resetComponents();
-    }
-
-    const resetComponents = () => {
-        labels.forEach(label => {
-            const icon = label.children[0];
-            icon.innerText = iconPending;
-            label.style.color = colorBlack;
-        });
-    };
-
-    const updateTitleModal = (index) => {
-        const titleModal = document.querySelector('#modal-ambos-container .modal-titulo span');
-
-        if (index > 4) titleModal.innerText = 'Ressonância';
-        if (index <= 4 && titleModal.innerText !== 'Raio X') titleModal.innerText = 'Raio X';
-    };
-
-    const updateVisibilityLabels = (index) => {
-        const labelsContainer = document.querySelector('#modal-ambos-container .labels')
-
-        if (index > 4) labelsContainer.style.display = 'none';
-        if (index <= 4 && labelsContainer.style.display === 'none') labelsContainer.style.display = 'block';
-    };
-
-    const updateVisibilityProgressBar = (index) => {
-        const progressTitle = document.querySelector('#modal-ambos-container #progress-title');
-        const progressBarContainer = document.querySelector('#modal-ambos-container .progress-bar')
-
-        if (index > 4) {
-            progressTitle.style.display = 'none';
-            progressBarContainer.style.display = 'none';
-        }
-
-        if (index <= 4 && progressTitle.style.display === 'none') {
-            progressTitle.style.display = 'block';
-            progressBarContainer.style.display = 'block';
-        }
-    }
-
-    const updateWidthModal = (index) => {
-        if (index > 4) modalAmbos.style.width = '550px';
-        if (index <= 4 && modalAmbos.style.width === '550px') modalAmbos.style.width = '700px';
-    }
-
-    const updateProgress = () => {
-        const percent = (currentStep / totalSteps) * 100;
-        progressBar.style.width = `${percent}%`;
-
-        if (currentStep === totalSteps) {
-            updateVisibilityButtons(currentStep);
-
-            btnNextStep.removeEventListener('click', nextElement);
-            btnNextStep.addEventListener('click', initModalProntuario);
-
-            fileNames[fileNames.length - 1].innerText = 'Todos os arquivos foram selecionados';
-        } else {
-            updateTextButtons();
-            updateVisibilityButtons(currentStep);
-        }
-    };
-
-    const updateVisibilityButtons = () => {
-        if (currentStep === 6) {
-            // btnNextStep.style.display = 'none';
-            // btnPrevStep.style.display = 'none';
-            // btnSubmit.style.display = 'block';
-        }
-    };
-
-    const updateTextButtons = () => {
-        if (currentStep < totalSteps) btnNextStep.innerText = 'Avançar';
-
-        if (currentStep > 0) btnPrevStep.innerText = 'Voltar';
-        else btnPrevStep.innerText = 'Fechar';
-    };
-
-    const updateEventBtnPrev = () => {
-        if (currentStep === 0) {
-            btnPrevStep.removeEventListener('click', prevElement);
-            btnPrevStep.addEventListener('click', closeModaBoth);
-        }
-        else {
-            btnPrevStep.removeEventListener('click', closeModaBoth);
-            btnPrevStep.addEventListener('click', prevElement);
-        }
-    }
-
-    const showCurrentInput = (index) => {
-        if (index < inputs.length - 1) {
-            const input = inputs[index];
-            input.style.visibility = 'visible';
-        }
-    };
-
-    const updateFileName = (index) => {
-        if (index < steps.length) {
-            const fileName = inputs[index].value;
-            fileNames[index].innerText = fileName.split('\\').pop() || 'Nenhum arquivo selecionado';
-        }
-    };
-
-    const showCurrentStep = (step) => {
-        steps.forEach(s => s.classList.remove('active'));
-
-        const activeStep = document.querySelector(`#modal-ambos-container .form-step[data-step="${step}"]`);
-
-        if (activeStep) activeStep.classList.add('active');
-
-        if (step > 4) activeStep.classList.add('ressonancia')
-        if (step <= 4) activeStep.classList.remove('ressonancia')
-
-        if (step >= steps.length) {
-            const lastStep = steps[steps.length - 1];
-            lastStep.classList.add('active');
-        }
-    };
-
-    const showCurrentLabel = (index) => {
-        if (index < 6) {
-            if (index === 0) {
-                const label = labels[index];
-                label.style.color = colorBlue;
-            }
-
-            if (index > 0) {
-                const prevLabel = labels[index - 1];
-                prevLabel.style.color = colorBlack;
-            }
-
-            if (index < labels.length && index !== 5) {
-                const currentLabel = labels[index];
-                currentLabel.style.color = colorBlue;
-            }
-
-            if (index < labels.length - 1) {
-                const nextLabel = labels[index + 1];
-                nextLabel.style.color = colorBlack;
-            }
-        }
-    };
-
-    const updateCurrentLabelIcon = (currentIndex) => {
-        labels.forEach((label, index) => {
-            const icon = label.children[0];
-            if (index < currentIndex) {
-                icon.innerText = iconCheck;
-                icon.style.color = colorGreen;
-            } else if (index === currentIndex) {
-                icon.innerText = iconPending;
-                icon.style.color = colorBlack;
-            } else {
-                icon.innerText = iconPending;
-                icon.style.color = colorBlack;
-            }
-        });
-    }
-
-    function nextElement() {
-        if (!inputs[currentStep].value) {
-            fileNames[currentStep].innerText = 'Selecione um arquivo antes de avançar';
-
-            setTimeout(() => {
-                fileNames[currentStep].innerText = 'Nenhum arquivo selecionado';
-            }, 1000);
-
-            return
-        }
-
-        if (currentStep < totalSteps) {
-            currentStep++;
-
-            showCurrentLabel(currentStep);
-            showCurrentStep(currentStep);
-            showCurrentInput(currentStep);
-            updateCurrentLabelIcon(currentStep);
-            updateTitleModal(currentStep);
-            updateVisibilityLabels(currentStep);
-            updateVisibilityProgressBar(currentStep);
-            updateWidthModal(currentStep);
-
-            if (stepProgressValue < inputs.length) {
-                stepProgressValue++;
-                valueProgress.innerHTML = `${stepProgressValue}`;
-            }
-
-            updateProgress();
-        }
-    }
-
-    function prevElement() {
-        if (currentStep > 0) {
-            currentStep--;
-
-            showCurrentLabel(currentStep)
-            showCurrentStep(currentStep);
-            showCurrentInput(currentStep)
-            updateTitleModal(currentStep);
-            updateVisibilityLabels(currentStep);
-            updateVisibilityProgressBar(currentStep);
-            updateWidthModal(currentStep);
-
-            if (stepProgressValue >= 1) {
-                stepProgressValue--;
-                valueProgress.innerHTML = `${stepProgressValue}`;
-            }
-
-            updateProgress();
-        }
-    }
-
-    function setInputPatientModalBoth() {
-        const formRegisterPatient = document.querySelector('#form-register-patient');
-        document.querySelector('#input-patient-modal-both').value = formRegisterPatient.getAttribute('data-patient')
-    }
-
-    function initModalProntuario() {
-        modalProntuarioAmbos.style.display = 'block';
-        modalAmbos.style.display = 'none';
-    }
+function openModalExamXRay(e) {
+  const btn = e?.currentTarget || e?.target;
+  const dataTarget = btn?.getAttribute("data-target-modal");
+  if (dataTarget !== "modal-both") {
+    return;
+  }
+  const modalXray = document.querySelector("#modal-exam-xray");
+  const backgroundModal = document.querySelector("#modal-backdrop");
+  const modalRegisterPatient = document.querySelector(
+    "#modal-register-patient-container"
+  );
+  if (modalRegisterPatient) closeCurrentModal(modalRegisterPatient.id);
+  if (modalXray && backgroundModal) {
+    modalXray.style.display = "block";
+    backgroundModal.classList.add("show");
+  }
+  setValueInputHidden();
 }
+
+function closeModalExamXRay() {
+  const modalXRay = document.querySelector("#modal-exam-xray");
+  if (modalXRay) modalXRay.style.display = "none";
+}
+
+function backToModalExamXray() {
+  const modalResonance = document.querySelector("#modal-exam-resonance");
+  const modalXray = document.querySelector("#modal-exam-xray");
+  closeCurrentModal(modalResonance.id);
+  modalXray.style.display = "block";
+}
+
+function openModalExamResonance() {
+  const modalResonance = document.querySelector("#modal-exam-resonance");
+  closeModalExamXRay();
+  modalResonance.style.display = "block";
+}
+
+function closeModalExamResonance() {
+  const modalResonance = document.querySelector("#modal-exam-resonance");
+  if (modalResonance) modalResonance.style.display = "none";
+}
+
+function backToModalResonance() {
+  const modalResonance = document.querySelector("#modal-exam-resonance");
+  const modalRecordBoth = document.querySelector(
+    "#modal-prontuario-container-ambos"
+  );
+  closeCurrentModal(modalRecordBoth.id);
+  modalResonance.style.display = "block";
+}
+
+function openModalRecord() {
+  const modalRecordBoth = document.querySelector(
+    "#modal-prontuario-container-ambos"
+  );
+  closeModalExamResonance();
+  if (modalRecordBoth) modalRecordBoth.style.display = "block";
+}
+
+function closeModalBoth() {
+  document.querySelectorAll(".modal-exam-send-images").forEach((modal) => {
+    modal.style.display = "none";
+  });
+  const inputs = document.querySelectorAll(
+    ".modal-exam-send-images input, #modal-prontuario-container-ambos input"
+  );
+  updateQuantityFiles(0);
+  inputs.forEach((input) => {
+    input.value = "";
+  });
+  const backgroundModal = document.querySelector("#modal-backdrop");
+  if (backgroundModal) backgroundModal.classList.remove("show");
+}
+
+function updateQuantityFiles(
+  quantityXRays = undefined,
+  quantityResonances = undefined
+) {
+  const quantityFilesXRay = document.querySelector(
+    "#modal-exam-xray #valor-input-ressonancia span"
+  );
+  const quantityFilesResonance = document.querySelector(
+    "#modal-exam-resonance #valor-input-ressonancia span"
+  );
+
+  if (quantityXRays != undefined)
+    quantityFilesXRay.innerText = `${quantityXRays}`;
+  if (quantityResonances != undefined)
+    quantityFilesResonance.innerText = `${quantityResonances}`;
+}
+
+function setValueInputHidden() {
+  const formRegisterPatient = document.querySelector("#form-register-patient");
+  const inputPatient = document.querySelector("#input-patient-modal-both");
+  const inputTypeExam = document.querySelector("#input-type-exam");
+
+  if (formRegisterPatient && inputPatient) {
+    console.log(formRegisterPatient.getAttribute("data-patient"));
+    inputPatient.value = formRegisterPatient.getAttribute("data-patient") || "";
+  }
+  if (inputTypeExam) inputTypeExam.value = "both";
+}
+
+function assignListenersModalBoth() {
+  const btnOpenModalXRayExam = document.querySelector(
+    "#btn-submit-modal-register-patient"
+  );
+  if (btnOpenModalXRayExam) {
+    btnOpenModalXRayExam.removeEventListener("click", openModalExamXRay);
+    btnOpenModalXRayExam.addEventListener("click", function (e) {
+      openModalExamXRay(e);
+    });
+  }
+
+  const btnOpenModalResonanceExam = document.querySelector(
+    "#btn-submit-images-xray"
+  );
+  if (btnOpenModalResonanceExam) {
+    btnOpenModalResonanceExam.removeEventListener(
+      "click",
+      openModalExamResonance
+    );
+    btnOpenModalResonanceExam.addEventListener("click", openModalExamResonance);
+  }
+
+  const btnOpenModalRecord = document.querySelector(
+    "#btn-submit-images-resonance"
+  );
+  if (btnOpenModalRecord) {
+    btnOpenModalRecord.removeEventListener("click", openModalRecord);
+    btnOpenModalRecord.addEventListener("click", openModalRecord);
+  }
+
+  const btnCloseModalBoth = document.querySelectorAll(
+    ".modal-exam-send-images .btn-fechar"
+  );
+  btnCloseModalBoth?.forEach((btn) => {
+    btn.removeEventListener("click", closeModalBoth);
+    btn.addEventListener("click", closeModalBoth);
+  });
+
+  const inputImages = document.querySelectorAll(
+    ".modal-exam-send-images .input-container-images input"
+  );
+  inputImages?.forEach((input) => {
+    input.removeEventListener("change", updateQuantityFiles);
+    input.removeEventListener("change", handleInputSubmitImages);
+    if (input.name === "images_xray") {
+      input.addEventListener("change", (e) =>
+        updateQuantityFiles(e.target.files.length)
+      );
+    } else {
+      input.addEventListener("change", (e) =>
+        updateQuantityFiles(null, e.target.files.length)
+      );
+    }
+    input.addEventListener("change", handleInputSubmitImages);
+  });
+
+  const btnBackModalImagesXRay = document.querySelector(
+    "#btn-back-images-xray"
+  );
+  if (btnBackModalImagesXRay) {
+    btnBackModalImagesXRay.removeEventListener("click", backToModalExamXray);
+    btnBackModalImagesXRay.addEventListener("click", backToModalExamXray);
+  }
+
+  const btnBackModalProntuario = document.querySelector("#btn-back-prontuario");
+  if (btnBackModalProntuario) {
+    btnBackModalProntuario.removeEventListener("click", backToModalResonance);
+    btnBackModalProntuario.addEventListener("click", backToModalResonance);
+  }
+}
+
+function handleInputSubmitImages(e) {
+  const modal = e.target.closest(".modal-exam-send-images");
+  if (!modal) return;
+  const btn = modal.querySelector('button.btn-primary[type="button"]');
+  if (!btn) return;
+  btn.disabled = e.target.files.length === 0;
+}
+
+document.addEventListener("DOMContentLoaded", assignListenersModalBoth);
+document.body.addEventListener("htmx:afterSwap", assignListenersModalBoth);
