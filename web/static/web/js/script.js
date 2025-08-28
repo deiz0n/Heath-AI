a1a3ed111d709c62ffb8720d2daabea172b11399
deiz0n <eduardomacedosilva@outlook.com>
2025-08-28 12:39:25 -0300

    refactor: comment out unused modal imports and clean up related code

--- web/static/web/js/script.js
+++ web/static/web/js/script.js
@@ -1,14 +1,9 @@
-import { startModalRaioX } from "./modal-raiox.js";
+// import { startModalRaioX } from "./modal-raiox.js";
 
-import { startModalAmbos } from "./modal-ambos.js";
-
-const btnStartRaioX = document.querySelectorAll('.btn-modal-raiox');
-const btnStartAmbos = document.querySelector('#btn-modal-ambos');
-
-if (btnStartRaioX) btnStartRaioX.forEach(btn => btn.addEventListener('click', startModalRaioX));
-if (btnStartAmbos) btnStartAmbos.addEventListener('click', startModalAmbos);
+const btnStartRaioX = document.querySelectorAll(".btn-modal-raiox");
+const btnStartAmbos = document.querySelector("#btn-modal-ambos");
 
 export function closeCurrentModal(modalId) {
-    const modal = document.querySelector(`#${modalId}`);
-    modal.style.display = 'none';
+  const modal = document.querySelector(`#${modalId}`);
+  modal.style.display = "none";
 }
