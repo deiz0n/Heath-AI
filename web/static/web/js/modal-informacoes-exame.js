c95cf17a12e4137afbc7854ba6a61983ec56b1df
deiz0n <eduardomacedosilva@outlook.com>
2025-08-28 09:08:13 -0300

    fix: ajuste ao verificar a existência de um prontuário para download

--- web/static/web/js/modal-informacoes-exame.js
+++ web/static/web/js/modal-informacoes-exame.js
@@ -1,89 +1,95 @@
 function initializeEventListeners() {
-    const patientDataContainer = document.querySelectorAll('.patient-data');
-    const modalInfoExam = document.querySelector('#modal-info-exam-container');
-    const btnClose = document.querySelector('#modal-info-exam-container i');
-    const backgroundModal = document.querySelector('#modal-backdrop');
+  const patientDataContainer = document.querySelectorAll(".patient-data");
+  const modalInfoExam = document.querySelector("#modal-info-exam-container");
+  const btnClose = document.querySelector("#modal-info-exam-container i");
+  const backgroundModal = document.querySelector("#modal-backdrop");
 
-    if (patientDataContainer.length) {
-        patientDataContainer.forEach(patientData => {
-            patientData.removeEventListener('click', function () {
-                setUrlButtons(this);
-                setDataModalInfoExam(this);
-                openModalInfoExam();
-            });
-            patientData.addEventListener('click', function () {
-                setUrlButtons(this);
-                setDataModalInfoExam(this);
-                openModalInfoExam();
-            });
-        });
+  if (patientDataContainer.length) {
+    patientDataContainer.forEach((patientData) => {
+      patientData.removeEventListener("click", function () {
+        setUrlButtons(this);
+        setDataModalInfoExam(this);
+        openModalInfoExam();
+      });
+      patientData.addEventListener("click", function () {
+        setUrlButtons(this);
+        setDataModalInfoExam(this);
+        openModalInfoExam();
+      });
+    });
+  }
+
+  if (btnClose) {
+    btnClose.removeEventListener("click", closeModalInfoExam);
+    btnClose.addEventListener("click", closeModalInfoExam);
+  }
+
+  function openModalInfoExam() {
+    modalInfoExam.style.display = "block";
+    backgroundModal.classList.add("show");
+  }
+
+  function closeModalInfoExam() {
+    modalInfoExam.style.display = "none";
+    backgroundModal.classList.remove("show");
+  }
+
+  function setDataModalInfoExam(patientData) {
+    const datasets = {
+      patientName: patientData.dataset.patientName,
+      patientCpf: patientData.dataset.patientCpf,
+      patientAge: patientData.dataset.patientAge,
+      patientAddress: patientData.dataset.patientAddress,
+      clinicianName: patientData.dataset.clinicianName,
+      clinicianCrm: patientData.dataset.clinicianCrm,
+      examType: patientData.dataset.examType,
+      examData: patientData.dataset.exam,
+      examId: patientData.dataset.examId,
+      resonance: patientData.dataset.examResonance,
+    };
+
+    document.querySelector("#patient-name").textContent = datasets.patientName;
+    document.querySelector("#patient-cpf").textContent = datasets.patientCpf;
+    document.querySelector(
+      "#patient-age"
+    ).textContent = `${datasets.patientAge} anos`;
+    document.querySelector("#patient-address").textContent =
+      datasets.patientAddress;
+    document.querySelector("#clinician-name").textContent =
+      datasets.clinicianName;
+    document.querySelector("#clinician-crm").textContent =
+      datasets.clinicianCrm;
+    document.querySelector("#exam-type").textContent = datasets.examType;
+    document.querySelector("#exam-data").textContent = datasets.examData;
+    document.querySelector("#modal-info-btn-show-images").dataset.examId =
+      datasets.examId;
+    document.querySelector("#modal-info-btn-show-images").dataset.resonanceId =
+      datasets.resonance;
+  }
+
+  function setUrlButtons(patientData) {
+    const examId = patientData.dataset.examId;
+    const btnDownload = document.querySelector("#modal-info-btn-download");
+
+    if (btnDownload && examId && existsExamRecord(patientData)) {
+      btnDownload.href = `/record_by_exam_id/${examId}`;
     }
+  }
 
-    if (btnClose) {
-        btnClose.removeEventListener('click', closeModalInfoExam);
-        btnClose.addEventListener('click', closeModalInfoExam);
+  function existsExamRecord(patientData) {
+    const examRecord = patientData.dataset.examRecord;
+    const btnDownload = document.querySelector("#modal-info-btn-download");
+
+    if (examRecord && examRecord !== "None") {
+      btnDownload.textContent = "Baixar prontuário";
+      return true;
+    } else {
+      btnDownload.textContent = "Enviar prontuário";
+      btnDownload.setAttribute("data-action", "upload");
+      return false;
     }
-
-    function openModalInfoExam() {
-        modalInfoExam.style.display = 'block';
-        backgroundModal.classList.add('show');
-    }
-
-    function closeModalInfoExam() {
-        modalInfoExam.style.display = 'none';
-        backgroundModal.classList.remove('show');
-    }
-
-    function setDataModalInfoExam(patientData) {
-        const datasets = {
-            patientName: patientData.dataset.patientName,
-            patientCpf: patientData.dataset.patientCpf,
-            patientAge: patientData.dataset.patientAge,
-            patientAddress: patientData.dataset.patientAddress,
-            clinicianName: patientData.dataset.clinicianName,
-            clinicianCrm: patientData.dataset.clinicianCrm,
-            examType: patientData.dataset.examType,
-            examData: patientData.dataset.exam,
-            examId: patientData.dataset.examId,
-            resonance: patientData.dataset.examResonance
-        };
-
-        document.querySelector(('#patient-name')).textContent = datasets.patientName;
-        document.querySelector(('#patient-cpf')).textContent = datasets.patientCpf;
-        document.querySelector(('#patient-age')).textContent = `${datasets.patientAge} anos`;
-        document.querySelector(('#patient-address')).textContent = datasets.patientAddress;
-        document.querySelector(('#clinician-name')).textContent = datasets.clinicianName;
-        document.querySelector(('#clinician-crm')).textContent = datasets.clinicianCrm;
-        document.querySelector(('#exam-type')).textContent = datasets.examType;
-        document.querySelector(('#exam-data')).textContent = datasets.examData;
-        document.querySelector('#modal-info-btn-show-images').dataset.examId = datasets.examId;
-        document.querySelector('#modal-info-btn-show-images').dataset.resonanceId = datasets.resonance;
-    }
-
-    function setUrlButtons(patientData) {
-        const examId = patientData.dataset.examId;
-        const btnDownload = document.querySelector('#modal-info-btn-download');
-
-        if (btnDownload && examId && existsExamRecord(patientData)) {
-            btnDownload.href = `/record_by_exam_id/${examId}`;
-        }
-    }
-
-    function existsExamRecord(patientData) {
-        const examRecord = patientData.dataset.examRecord;
-        const btnDownload = document.querySelector('#modal-info-btn-download');
-
-        if (examRecord !== 'None') {
-            btnDownload.textContent = 'Baixar prontuário';
-            return true;
-        }
-        else {
-            btnDownload.textContent = "Enviar prontuário";
-            btnDownload.setAttribute('data-action', 'upload');
-            return false;
-        }
-    }
+  }
 }
 
-document.addEventListener('DOMContentLoaded', initializeEventListeners);
-document.body.addEventListener('htmx:afterSwap', initializeEventListeners);
+document.addEventListener("DOMContentLoaded", initializeEventListeners);
+document.body.addEventListener("htmx:afterSwap", initializeEventListeners);
