26ff9034aeada810b46afd892c69de9a7faa7537
deiz0n <eduardomacedosilva@outlook.com>
2025-09-16 10:24:53 -0300

    feat: enhance image processing for X-Ray and Resonance exams with DICOM conversion and logging

--- web/views/upload_views.py
+++ web/views/upload_views.py
@@ -10,6 +10,7 @@ from web.forms import (
     XRayForm
 )
 from web.models import ExamType,Exam, Clinician, Patient
+from web.utils.upload_processors import process_multiple_images, save_conversion_log, is_conversion_enabled
 
 
 from datetime import date
@@ -46,15 +47,34 @@ class UploadMultiModalRequestView(LoginRequiredMixin, View):
                 xray = xray_form.save(commit=False)
                 xray.save()
 
-                imgs_xray = []
-                for f in files.getlist('images_xray'):  
-                    imgs_xray.append(
-                        ImagesXRayForm(
+                # Processa imagens de X-Ray com conversão DICOM automática
+                xray_files = files.getlist('images_xray')
+                
+                if is_conversion_enabled():
+                    processed_results = process_multiple_images(xray_files, "xray")
+                    
+                    imgs_xray = []
+                    for processed_file, metadata in processed_results:
+                        img_obj = ImagesXRayForm(
                             {'xray': xray.id},
-                            {'image': f}
+                            {'image': processed_file}
                         ).save(commit=False)
-                    )
-                ImagesXRayForm.Meta.model.objects.bulk_create(imgs_xray)
+                        imgs_xray.append(img_obj)
+                        
+                    
+                    ImagesXRayForm.Meta.model.objects.bulk_create(imgs_xray)
+                    
+                else:
+
+                    imgs_xray = []
+                    for f in xray_files:  
+                        imgs_xray.append(
+                            ImagesXRayForm(
+                                {'xray': xray.id},
+                                {'image': f}
+                            ).save(commit=False)
+                        )
+                    ImagesXRayForm.Meta.model.objects.bulk_create(imgs_xray)
 
             if exam_kind in ('resonance','both'):
                 res_form = ResonanceForm(request.POST, request.FILES)
@@ -67,15 +87,39 @@ class UploadMultiModalRequestView(LoginRequiredMixin, View):
                 resonance = res_form.save(commit=False)
                 resonance.save()
 
-                imgs_res = []
-                for f in files.getlist('images_resonance'):
-                    imgs_res.append(
-                        ResonanceImagesForm(
+                # Processa imagens de Ressonância com conversão DICOM automática
+                resonance_files = files.getlist('images_resonance')
+                
+                if is_conversion_enabled():
+                    processed_results = process_multiple_images(resonance_files, "resonance")
+                    
+                    imgs_res = []
+                    for processed_file, metadata in processed_results:
+                        img_obj = ResonanceImagesForm(
                             {'resonance': resonance.id},
-                            {'image': f}
+                            {'image': processed_file}
                         ).save(commit=False)
-                    )
-                ResonanceImagesForm.Meta.model.objects.bulk_create(imgs_res)
+                        imgs_res.append(img_obj)
+                        
+                        # Log da conversão se houver
+                    
+                    ResonanceImagesForm.Meta.model.objects.bulk_create(imgs_res)
+                    
+                    # Salva logs de conversão para todas as imagens
+                    for img_obj, (_, metadata) in zip(imgs_res, processed_results):
+                        save_conversion_log(img_obj, metadata)
+                        
+                else:
+                    # Processamento original sem conversão
+                    imgs_res = []
+                    for f in resonance_files:
+                        imgs_res.append(
+                            ResonanceImagesForm(
+                                {'resonance': resonance.id},
+                                {'image': f}
+                            ).save(commit=False)
+                        )
+                    ResonanceImagesForm.Meta.model.objects.bulk_create(imgs_res)
 
             exam = Exam(
                 type      = exam_type,
