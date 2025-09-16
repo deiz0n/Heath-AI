d975fc1c13f939880e3e63555f380229a798416e
deiz0n <eduardomacedosilva@outlook.com>
2025-09-16 10:30:30 -0300

    refactor: improve modal registration logic and enhance button target setting

--- web/static/web/js/modal-cadastro-paciente.js
+++ web/static/web/js/modal-cadastro-paciente.js
@@ -1,92 +1,118 @@
-import {closeCurrentModal} from "./script.js";
+import { closeCurrentModal } from "./script.js";
 
 function initializeModalRegisterPatient() {
-    const btnOpenModalXRay = document.querySelectorAll('.btn-modal-raiox');
-    const btnOpenModalResonance = document.querySelectorAll('.btn-modal-ressonancia');
-    const btnOpenModalBoth = document.querySelector('#btn-modal-ambos');
-    const btnOpenNextModal = document.querySelector('#btn-submit-modal-register-patient');
+  const btnOpenModalXRay = document.querySelectorAll(".btn-modal-raiox");
+  const btnOpenModalResonance = document.querySelectorAll(
+    ".btn-modal-ressonancia"
+  );
+  const btnOpenModalBoth = document.querySelector("#btn-modal-ambos");
+  const btnOpenNextModal = document.querySelector(
+    "#btn-submit-modal-register-patient"
+  );
 
-    const btnCloseModalRegisterPatient = document.querySelector('#modal-register-patient-container i');
-    const btnCloseBottomRegisterPatient = document.querySelector('#btn-close-modal-register-patient');
+  const btnCloseModalRegisterPatient = document.querySelector(
+    "#modal-register-patient-container i"
+  );
+  const btnCloseBottomRegisterPatient = document.querySelector(
+    "#btn-close-modal-register-patient"
+  );
 
-    const background = document.querySelector('#modal-backdrop');
-    const modalOptions2D = document.querySelector('#modal-opcoes-2d-container');
-    const modalOptions3D = document.querySelector('#modal-opcoes-3d-container');
-    const modalRegisterPatient = document.querySelector('#modal-register-patient-container');
+  const background = document.querySelector("#modal-backdrop");
+  const modalOptions2D = document.querySelector("#modal-opcoes-2d-container");
+  const modalOptions3D = document.querySelector("#modal-opcoes-3d-container");
+  const modalRegisterPatient = document.querySelector(
+    "#modal-register-patient-container"
+  );
 
-    const inputCpf = document.querySelector('#modal-register-patient-container input[name=cpf]')
+  const inputCpf = document.querySelector(
+    "#modal-register-patient-container input[name=cpf]"
+  );
 
-    const formRegisterPatient = document.querySelector('#form-register-patient');
+  const formRegisterPatient = document.querySelector("#form-register-patient");
 
-    inputCpf?.addEventListener('input', (e) => formatterCpf(e.target));
+  inputCpf?.addEventListener("input", (e) => formatterCpf(e.target));
 
-    if (btnOpenModalXRay || btnOpenModalResonance || btnOpenModalBoth) {
-        btnOpenModalXRay?.forEach(btn =>
-            btn.addEventListener('click', (e) => {
-                openModalRegisterPatient();
-                setBtnOpenNextModal(e.target);
-            })
-        )
+  if (btnOpenModalXRay || btnOpenModalResonance || btnOpenModalBoth) {
+    btnOpenModalXRay?.forEach((btn) =>
+      btn.addEventListener("click", (e) => {
+        openModalRegisterPatient();
+        setBtnOpenNextModal(e.target);
+      })
+    );
 
-        btnOpenModalResonance?.forEach(btn =>
-            btn.addEventListener('click', (e) => {
-                openModalRegisterPatient();
-                setBtnOpenNextModal(e.target);
-            })
-        )
+    btnOpenModalResonance?.forEach((btn) =>
+      btn.addEventListener("click", (e) => {
+        openModalRegisterPatient();
+        setBtnOpenNextModal(e.target);
+      })
+    );
 
-        btnOpenModalBoth?.addEventListener('click', (e) => {
-            openModalRegisterPatient();
-            setBtnOpenNextModal(e.target);
-        });
-    }
+    btnOpenModalBoth?.addEventListener("click", (e) => {
+      openModalRegisterPatient();
+      setBtnOpenNextModal(e.target);
+    });
+  }
 
-    if (btnCloseModalRegisterPatient && btnCloseBottomRegisterPatient) {
-        btnCloseBottomRegisterPatient.addEventListener('click', closeModalRegisterPatient)
+  if (btnCloseModalRegisterPatient && btnCloseBottomRegisterPatient) {
+    btnCloseBottomRegisterPatient.addEventListener(
+      "click",
+      closeModalRegisterPatient
+    );
 
-        btnCloseBottomRegisterPatient.addEventListener('click', closeModalRegisterPatient)
-    }
+    btnCloseBottomRegisterPatient.addEventListener(
+      "click",
+      closeModalRegisterPatient
+    );
+  }
 
-    function openModalRegisterPatient() {
-        if (modalOptions2D.style.display !== 'none') closeCurrentModal(modalOptions2D.id)
-        if (modalOptions3D.style.display !== 'none') closeCurrentModal(modalOptions3D.id)
+  function openModalRegisterPatient() {
+    if (modalOptions2D.style.display !== "none")
+      closeCurrentModal(modalOptions2D.id);
+    if (modalOptions3D.style.display !== "none")
+      closeCurrentModal(modalOptions3D.id);
 
-        modalRegisterPatient.style.display = 'block';
-    }
+    modalRegisterPatient.style.display = "block";
+  }
 
-    function closeModalRegisterPatient() {
-        modalRegisterPatient.style.display = 'none';
-        background.classList.remove('show');
-    }
+  function closeModalRegisterPatient() {
+    modalRegisterPatient.style.display = "none";
+    background.classList.remove("show");
+  }
 
-    function formatterCpf(input) {
-        let cpfValue = input.value.replace(/\D/g, '');
+  function formatterCpf(input) {
+    let cpfValue = input.value.replace(/\D/g, "");
 
-        if (cpfValue.length > 11) cpfValue = cpfValue.substring(0, 11);
+    if (cpfValue.length > 11) cpfValue = cpfValue.substring(0, 11);
 
-        let valueFormatted = cpfValue;
+    let valueFormatted = cpfValue;
 
-        if (cpfValue.length > 9) valueFormatted = cpfValue.replace(/(\d{3})(\d{3})(\d{3})(\d{1,2})/, "$1.$2.$3-$4");
-        else if (cpfValue.length > 6) valueFormatted = cpfValue.replace(/(\d{3})(\d{3})(\d{1,3})/, "$1.$2.$3");
-        else if (cpfValue.length > 3) valueFormatted = cpfValue.replace(/(\d{3})(\d{1,3})/, "$1.$2");
+    if (cpfValue.length > 9)
+      valueFormatted = cpfValue.replace(
+        /(\d{3})(\d{3})(\d{3})(\d{1,2})/,
+        "$1.$2.$3-$4"
+      );
+    else if (cpfValue.length > 6)
+      valueFormatted = cpfValue.replace(/(\d{3})(\d{3})(\d{1,3})/, "$1.$2.$3");
+    else if (cpfValue.length > 3)
+      valueFormatted = cpfValue.replace(/(\d{3})(\d{1,3})/, "$1.$2");
 
-        input.value = valueFormatted;
-        formRegisterPatient.setAttribute('data-patient', valueFormatted);
-    }
+    input.value = valueFormatted;
+    formRegisterPatient.setAttribute("data-patient", valueFormatted);
+  }
 
-    function setBtnOpenNextModal(btnTarget) {
-        if (btnTarget.id === 'btn-modal-ambos')
-            btnOpenNextModal.setAttribute('data-target-modal', 'modal-both')
-        if (btnTarget.id === 'btn-modal-x-ray-2d')
-            btnOpenNextModal.setAttribute('data-target-modal', 'modal-x-ray-2d')
-        if (btnTarget.id === 'btn-modal-resonance-2d')
-            btnOpenNextModal.setAttribute('data-target-modal', 'modal-resonance-2d')
-        if (btnTarget.id === 'btn-modal-x-ray-3d')
-            btnOpenNextModal.setAttribute('data-target-modal', 'modal-x-ray-3d')
-        if (btnTarget.id === 'btn-modal-resonance-3d')
-            btnOpenNextModal.setAttribute('data-target-modal', 'modal-resonance-3d')
-    }
+  function setBtnOpenNextModal(btnTarget) {
+    if (btnTarget.id.includes("ambos"))
+      btnOpenNextModal.setAttribute("data-target-modal", "modal-both");
+    else if (btnTarget.id.includes("x-ray"))
+      btnOpenNextModal.setAttribute("data-target-modal", "modal-x-ray");
+    else if (btnTarget.id.includes("resonance"))
+      btnOpenNextModal.setAttribute("data-target-modal", "modal-resonance");
+    console.log(btnOpenNextModal.getAttribute("data-target-modal"));
+  }
 }
 
-document.addEventListener('DOMContentLoaded', initializeModalRegisterPatient);
-document.body.addEventListener('htmx:afterSwap', initializeModalRegisterPatient);
+document.addEventListener("DOMContentLoaded", initializeModalRegisterPatient);
+document.body.addEventListener(
+  "htmx:afterSwap",
+  initializeModalRegisterPatient
+);
